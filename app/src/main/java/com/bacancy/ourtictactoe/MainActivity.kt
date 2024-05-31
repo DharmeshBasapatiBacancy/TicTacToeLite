2955b991f071bbc1214da95b9d764af7c1d984e2
Dharmesh Basapati Bacancy <dharmesh.basapati@bacancy.com>
2024-05-31 12:24:36 +0530

    - set text on view to X if returning to the false - Give to Rahul

--- app/src/main/java/com/bacancy/ourtictactoe/MainActivity.kt
+++ app/src/main/java/com/bacancy/ourtictactoe/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bacancy.ourtictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.bacancy.ourtictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,47 +23,48 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             tvBox1.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox1.text,setCurrentValueToBox)
+                tvBox1.text = checkGameStatus(tvBox1.text,setCurrentValueToBox)
+                currentEntry = setCurrentValueToBox
             }
 
             tvBox2.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox2.text, setCurrentValueToBox)
+                tvBox2.text =  checkGameStatus(tvBox2.text, setCurrentValueToBox)
             }
 
             tvBox3.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox3.text, setCurrentValueToBox)
+                tvBox3.text = checkGameStatus(tvBox3.text, setCurrentValueToBox)
             }
 
             tvBox4.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox4.text, setCurrentValueToBox)
+                tvBox4.text = checkGameStatus(tvBox4.text, setCurrentValueToBox)
             }
 
             tvBox5.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox5.text, setCurrentValueToBox)
+                tvBox5.text = checkGameStatus(tvBox5.text, setCurrentValueToBox)
             }
 
             tvBox6.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox6.text, setCurrentValueToBox)
+                tvBox6.text = checkGameStatus(tvBox6.text, setCurrentValueToBox)
             }
 
             tvBox7.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox7.text, setCurrentValueToBox)
+                tvBox7.text = checkGameStatus(tvBox7.text, setCurrentValueToBox)
             }
 
             tvBox8.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox8.text, setCurrentValueToBox)
+                tvBox8.text = checkGameStatus(tvBox8.text, setCurrentValueToBox)
             }
 
             tvBox9.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
-                checkGameStatus(tvBox9.text, setCurrentValueToBox)
+                tvBox9.text = checkGameStatus(tvBox9.text, setCurrentValueToBox)
             }
 
             btnStartResetGame.setOnClickListener {
@@ -73,13 +75,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkGameStatus(text: CharSequence, setCurrentValueToBox: String) {
-//        if (setCurrentValueToBox.equals("FALSE"))
-//        {
-//            //set value to "X" if False
-//        }else{
-//            //set value to "0" if TRUE
-//        }
+    private fun checkGameStatus(text: CharSequence, setCurrentValueToBox: String) : String {
+        return if (setCurrentValueToBox.equals("FALSE")) {
+            //set value to "X" if False
+            Log.e("TAG", "checkGameStatus: $text $setCurrentValueToBox", )
+            "X"
+        }else{
+            //set value to "0" if TRUE
+            Log.e("TAG", "checkGameStatus: $text $setCurrentValueToBox", )
+            "0"
+        }
     }
 
     private fun checkCurrentEntry(): String {
