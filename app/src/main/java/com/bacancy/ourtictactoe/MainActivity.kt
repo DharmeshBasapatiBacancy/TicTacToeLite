933375a69cdb3e7c1967d7c3b1676427431c8176
Dharmesh Basapati Bacancy <dharmesh.basapati@bacancy.com>
2024-05-31 13:03:48 +0530

    - won and reset changes applied
    by keval

--- app/src/main/java/com/bacancy/ourtictactoe/MainActivity.kt
+++ app/src/main/java/com/bacancy/ourtictactoe/MainActivity.kt
@@ -33,48 +33,56 @@ class MainActivity : AppCompatActivity() {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox2.text =  checkGameStatus(tvBox2.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox3.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox3.text = checkGameStatus(tvBox3.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox4.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox4.text = checkGameStatus(tvBox4.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox5.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox5.text = checkGameStatus(tvBox5.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox6.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox6.text = checkGameStatus(tvBox6.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox7.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox7.text = checkGameStatus(tvBox7.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox8.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox8.text = checkGameStatus(tvBox8.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             tvBox9.setOnClickListener {
                 setCurrentValueToBox = checkCurrentEntry()
                 tvBox9.text = checkGameStatus(tvBox9.text, setCurrentValueToBox)
                 currentEntry = setCurrentValueToBox
+                checkWinningStatus()
             }
 
             btnStartResetGame.setOnClickListener {
@@ -88,7 +96,7 @@ class MainActivity : AppCompatActivity() {
                 tvBox7.text="";
                 tvBox8.text="";
                 tvBox9.text="";
-
+                tvPlayerTurn.text="";
             }
 
         }
@@ -108,6 +116,42 @@ class MainActivity : AppCompatActivity() {
             } else if (checkForWinner(tvBox4, tvBox5, tvBox6, symbol = "0")) {
                 tvPlayerTurn.text = "0 is won"
             }
+
+            if (checkForWinner(tvBox7, tvBox8, tvBox9, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox7, tvBox8, tvBox9, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
+
+            if (checkForWinner(tvBox1, tvBox4, tvBox7, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox1, tvBox4, tvBox7, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
+
+            if (checkForWinner(tvBox2, tvBox5, tvBox8, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox2, tvBox5, tvBox8, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
+
+            if (checkForWinner(tvBox3, tvBox6, tvBox9, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox3, tvBox6, tvBox9, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
+
+            if (checkForWinner(tvBox1, tvBox5, tvBox9, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox1, tvBox5, tvBox9, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
+
+            if (checkForWinner(tvBox3, tvBox5, tvBox7, symbol = "X")) {
+                tvPlayerTurn.text = "X is won"
+            } else if (checkForWinner(tvBox7, tvBox8, tvBox9, symbol = "0")) {
+                tvPlayerTurn.text = "0 is won"
+            }
         }
     }
 
@@ -116,7 +160,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkGameStatus(text: CharSequence, setCurrentValueToBox: String) : String {
-        return listOf("X", "0").random()
+//        return listOf("X", "0").random()
+        return if (setCurrentValueToBox == "FALSE") {
+            //set value to "X" if False
+            Log.e("TAG", "checkGameStatus: $text $setCurrentValueToBox", )
+            "X"
+        }else{
+            //set value to "0" if TRUE
+            Log.e("TAG", "checkGameStatus: $text $setCurrentValueToBox", )
+            "0"
+        }
     }
 
     private fun checkCurrentEntry(): String {
